3700bc922a98d15a8c1b764a3b30db44cffcede5
xanabas <49243943+xanabas@users.noreply.github.com>
2021-05-15 04:30:27 +0100

    Add files via upload
    This is initial file

---
+++ basicCalculator.py
@@ -0,0 +1,25 @@
+import random
+a = int(input('''which operation do you want to do?
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Timestable
+'''))
+print(type(a))
+
+if a == 1:
+    print("add")
+elif a == 2:
+    print("subtract")
+elif a == 3:
+    print("multiply")
+elif a == 4:
+    print("Divide")
+elif a == 5:
+    b = int(input("Enter number to display timestable till 10:   "))
+    for x in range(11):
+        print(b*x)
+        
+else:
+    print("Invalid Operation")
